7ab200b46e84c9edb95b9b2e4e306aeee5db69e9
condolent <tacco_9@hotmail.com>
2020-08-31 19:07:24 +0200

    Added possibility for refresh tokens

--- src/main/java/com/jonteohr/tejbz/credentials/Credentials.java
+++ src/main/java/com/jonteohr/tejbz/credentials/Credentials.java
@@ -1,22 +1,28 @@
 package com.jonteohr.tejbz.credentials;
 
 public enum Credentials {
-	TOKEN("DISCORD_TOKEN"),
-	OAUTH("TEJBZ_OAUTH"),
-	BOTOAUTH("PGDABOT_OAUTH"),
-	DB_HOST("localhost"),
-	DB_NAME("db_name"),
-	DB_USER("db_user"),
-	DB_PASS("db_passwd");
+	TOKEN("DISCORD_TOKEN", ""),
+	OAUTH("TEJBZ_OAUTH", "TEJBZ_OAUTH_REFRESH"),
+	BOTOAUTH("PGDABOT_OAUTH", ""),
+	DB_HOST("localhost", ""),
+	DB_NAME("db_name", ""),
+	DB_USER("db_user", ""),
+	DB_PASS("db_passwd", "");
 	
-	private String val;
+	private String accessToken;
+	private String refreshToken;
 
-	Credentials(String string) {
-		this.val = string;
+	Credentials(String accessToken, String refreshToken) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
 	}
 	
 	public String getValue() {
-		return val;
+		return accessToken;
+	}
+	
+	public String getRefreshToken() {
+		return refreshToken;
 	}
 	
 }
