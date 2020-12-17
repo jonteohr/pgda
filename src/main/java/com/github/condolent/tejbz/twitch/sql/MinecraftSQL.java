d01e25b7e506d7612d77b5dcb3a9685a3ecedb64
condolent <tacco_9@hotmail.com>
2020-12-18 00:31:32 +0100

    Connect to minecraft server

---
+++ src/main/java/com/github/condolent/tejbz/twitch/sql/MinecraftSQL.java
@@ -0,0 +1,31 @@
+package com.github.condolent.tejbz.twitch.sql;
+
+import com.github.condolent.tejbz.credentials.Credentials;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class MinecraftSQL {
+	public static boolean connectToMinecraft(String user, String providedName) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://" + Credentials.DB_HOST.getValue() + ":3306/tejbz_mc?serverTimezone=UTC",
+					Credentials.DB_USER.getValue(),
+					Credentials.DB_PASS.getValue());
+
+			PreparedStatement pstmt = con.prepareStatement("INSERT INTO players(name,twitch) VALUES (?,?)");
+			pstmt.setString(1, providedName);
+			pstmt.setString(2, user);
+
+			pstmt.executeUpdate();
+
+			con.close();
+
+			return true;
+		} catch (Exception e) {
+			System.out.println(e);
+			return false;
+		}
+	}
+}
