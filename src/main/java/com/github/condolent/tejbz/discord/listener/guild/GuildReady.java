3cae77f2675c33f06c26562f0bed25c2fdc1ec87
Jonathan Öhrström <tacco_9@hotmail.com>
2021-02-10 09:24:03 +0100

    Default roles assigned to new members (#13)
    * Initial work on default roles
    
    * Better logging
    
    * Added role request system
    
    * Added supporter role support

--- src/main/java/com/github/condolent/tejbz/discord/listener/guild/GuildReady.java
+++ src/main/java/com/github/condolent/tejbz/discord/listener/guild/GuildReady.java
@@ -1,48 +1,68 @@
 package com.github.condolent.tejbz.discord.listener.guild;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import com.github.condolent.tejbz.App;
 import com.github.condolent.tejbz.discord.listener.commands.Join;
+import com.github.condolent.tejbz.discord.listener.roles.DefaultRoles;
 import com.github.condolent.tejbz.twitch.Twitch;
-
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GuildReady extends ListenerAdapter {
-	
+
 	public void onGuildReady(GuildReadyEvent e) {
 		Twitch.initTwitch();
-		
+
 		App.guild = e.getGuild();
 		App.general = e.getGuild().getTextChannelById("124204242683559938");
 		App.twitchLog = e.getGuild().getTextChannelById("735122823017791578");
 		Join.lobby = App.guild.getVoiceChannelById("124204246815080449");
 		Join.queue = App.guild.getVoiceChannelById("732569438326489139");
 		Join.live = App.guild.getVoiceChannelById("280730003484835860");
-		
+
 		setPresence(Twitch.getSubscribers("tejbz"));
-		
+
 		presenceTimer();
+
+		// Set the role variables
+		DefaultRoles.setRoles(e.getGuild());
+
+		// Role requests
+		RoleRequest.roleRequest = e.getGuild().getTextChannelById("808783594020798514");
+		RoleRequest.regionRequestMessage = RoleRequest.roleRequest.retrieveMessageById("808948588360106044").complete();
+		RoleRequest.gameRequestMessage = RoleRequest.roleRequest.retrieveMessageById("808960926992039937").complete();
+		RoleRequest.roleEU = e.getGuild().getRoleById("808958342277496842");
+		RoleRequest.roleNA = e.getGuild().getRoleById("808958423618027530");
+		RoleRequest.roleSA = e.getGuild().getRoleById("808958497784987688");
+		RoleRequest.roleAS = e.getGuild().getRoleById("808958387696959550");
+		RoleRequest.gameMc = e.getGuild().getRoleById("808964559292858378");
+		RoleRequest.gameMisc = e.getGuild().getRoleById("808964641312210964");
+		RoleRequest.gameRl = e.getGuild().getRoleById("808964875396841483");
+		RoleRequest.gameSot = e.getGuild().getRoleById("808964794341916674");
+		RoleRequest.gameWz = e.getGuild().getRoleById("808964608579731485");
+
+//		RoleRequest.editmsg();
+//		RoleRequest.sendMsg();
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param subs a {@link java.lang.Integer Integer} of total member count.
 	 */
 	public void setPresence(int subs) {
 		App.jda.getPresence().setActivity(Activity.watching(subs + " subs | Twitch.tv/Tejbz"));
 	}
-	
+
 	public void presenceTimer() {
 		Timer timer = new Timer();
-		
-		
-		
+
+
+
 		timer.scheduleAtFixedRate(new TimerTask() {
-			
+
 			@Override
 			public void run() {
 				setPresence(Twitch.getSubscribers("tejbz"));
