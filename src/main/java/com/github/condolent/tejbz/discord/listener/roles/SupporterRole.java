ec568a762e7e87a6e229434242a2be8ea674ff42
jonteohr <tacco_9@hotmail.com>
2021-02-11 09:22:10 +0100

    Cleaned up default roles

--- src/main/java/com/github/condolent/tejbz/discord/listener/roles/SupporterRole.java
+++ src/main/java/com/github/condolent/tejbz/discord/listener/roles/SupporterRole.java
@@ -7,12 +7,16 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class SupporterRole extends ListenerAdapter {
 	public void onGuildMemberRoleAdd(GuildMemberRoleAddEvent e) {
 		if(e.getRoles().contains(e.getGuild().getRoleById("278439657358753792")) || e.getRoles().contains(e.getGuild().getRoleById("640863929039323146"))) {
+			e.getGuild().addRoleToMember(e.getMember(), DefaultRoles.supporterDivider).complete();
+			e.getGuild().addRoleToMember(e.getMember(), DefaultRoles.tierDivider).complete();
 			e.getGuild().addRoleToMember(e.getMember(), e.getGuild().getRoleById("808968542007197707")).complete();
 		}
 	}
 
 	public void onGuildMemberRoleRemove(GuildMemberRoleRemoveEvent e) {
 		if(e.getRoles().contains(e.getGuild().getRoleById("278439657358753792")) || e.getRoles().contains(e.getGuild().getRoleById("640863929039323146"))) {
+			e.getGuild().removeRoleFromMember(e.getMember(), DefaultRoles.supporterDivider).complete();
+			e.getGuild().removeRoleFromMember(e.getMember(), DefaultRoles.tierDivider).complete();
 			e.getGuild().removeRoleFromMember(e.getMember(), e.getGuild().getRoleById("808968542007197707")).complete();
 		}
 	}
