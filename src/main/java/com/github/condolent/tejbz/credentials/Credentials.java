833b335d311d6ba1ca676cea47fa893e29f9b014
Jonathan Öhrström <tacco_9@hotmail.com>
2021-04-05 15:05:05 +0200

    Update Credentials.java

--- src/main/java/com/github/condolent/tejbz/credentials/Credentials.java
+++ src/main/java/com/github/condolent/tejbz/credentials/Credentials.java
@@ -3,11 +3,12 @@ package com.github.condolent.tejbz.credentials;
 public enum Credentials {
 	TOKEN("DISCORD_TOKEN", ""),
 	BOTOAUTH("PGDABOT_OAUTH", ""),
+	HYPROAUTH("", ""),
 	DB_HOST("localhost", ""),
 	DB_NAME("db_name", ""),
 	DB_USER("db_user", ""),
 	DB_PASS("db_passwd", "");
-	
+
 	private String accessToken;
 	private String refreshToken;
 
@@ -15,13 +16,13 @@ public enum Credentials {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
 	}
-	
+
 	public String getValue() {
 		return accessToken;
 	}
-	
+
 	public String getRefreshToken() {
 		return refreshToken;
 	}
-	
+
 }
